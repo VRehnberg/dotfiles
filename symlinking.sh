62886558119534e40d9a89be1a7915b342be1885
vikren <viktor.rehnberg@gmail.com>
2019-09-05 13:54:54 +0200

    added file for symlinking

---
+++ symlinking.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+ln -sv ~/Projects/dotfiles/.bash_aliases ~
+ln -sv ~/Projects/dotfiles/.vimrc ~
+ln -sv ~/Projects/dotfiles/.gitconfig ~
+ln -sv ~/Projects/dotfiles/.gitignore_global ~
